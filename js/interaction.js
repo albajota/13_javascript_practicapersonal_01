6ab235ffd2456a2543b7161c2677e23b495e7769
albajota <55701177+albajota@users.noreply.github.com>
2020-02-26 12:32:08 +0100

    Add files via upload

---
+++ js/interaction.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+            $("img").hide();
+            $("li:first-child a").click(function(){
+                $("li:first-child img").fadeIn();
+            });
+            $("li:first-child a").dblclick(function(){
+                $("li:first-child img").fadeOut();
+            });
+            $("li:nth-child(2) a").click(function(){
+                $("li:nth-child(2) img").fadeIn();
+            });
+            $("li:nth-child(2) a").dblclick(function(){
+                $("li:nth-child(2) img").fadeOut();
+            });
+            $("li:nth-child(3) a").click(function(){
+                $("li:nth-child(3) img").fadeIn();
+            });
+            $("li:nth-child(3) a").dblclick(function(){
+                $("li:nth-child(3) img").fadeOut();
+            });
+            $("li:nth-child(4) a").click(function(){
+                $("li:nth-child(4) img").fadeIn();
+            });
+            $("li:nth-child(4) a").dblclick(function(){
+                $("li:nth-child(4) img").fadeOut();
+            });
+        });
